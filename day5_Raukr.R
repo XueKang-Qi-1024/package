cf66763f665cab3963f0b95418fffbc35e3e9e85
xuekang.qi <XUEKANG.QI@KI.SE>
2025-06-13 10:27:17 +0200

    update

--- day5_Raukr.R
+++
@@ -1,8 +0,0 @@
-# load devtools, and check the version of the devtools package in R.
-library(devtools)
-packageVersion("devtools")
-
-# create_package
-create_package(
-  "~/Library/CloudStorage/OneDrive-KarolinskaInstitutet/Desktop/Raukr_local/package"
-)
